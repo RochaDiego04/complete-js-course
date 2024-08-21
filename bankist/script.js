b9fe1cad6e601eebcc951b9ada155312e006fa59
Diego Rocha <drocha@griddynamics.com>
2024-08-21 10:57:03 -0600

    Formatting dates to display x days ago instead of the complete date

--- bankist/script.js
+++ bankist/script.js
@@ -22,7 +22,7 @@ const account1 = {
     "2020-05-08T14:11:59.604Z",
     "2020-05-27T17:01:17.194Z",
     "2020-07-11T23:36:17.929Z",
-    "2020-07-12T10:51:36.790Z",
+    "2024-08-21T10:51:36.790Z",
   ],
   currency: "EUR",
   locale: "pt-PT", // de-DE
@@ -35,14 +35,14 @@ const account2 = {
   pin: 2222,
 
   movementsDates: [
-    "2019-11-01T13:15:33.035Z",
+    "2019-11-01T13:15:33.035Z", // z stands for the timezone.
     "2019-11-30T09:48:16.867Z",
     "2019-12-25T06:04:23.907Z",
     "2020-01-25T14:18:46.235Z",
     "2020-02-05T16:33:06.386Z",
     "2020-04-10T14:43:26.374Z",
-    "2020-06-25T18:49:59.371Z",
-    "2020-07-26T12:01:20.894Z",
+    "2024-08-18T18:49:59.371Z",
+    "2024-08-20T12:01:20.894Z",
   ],
   currency: "USD",
   locale: "en-US",
@@ -76,6 +76,23 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
+const formatMovementDate = function (date) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+
+  if (daysPassed === 0) return "Today";
+  if (daysPassed === 1) return "Yesterday";
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  else {
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
+};
+
 const displayMovements = function (currentAccount, sort = false) {
   containerMovements.innerHTML = "";
 
@@ -87,10 +104,8 @@ const displayMovements = function (currentAccount, sort = false) {
     const type = mov > 0 ? "deposit" : "withdrawal";
 
     const date = new Date(currentAccount.movementsDates[i]);
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatMovementDate(date);
+
     const html = `
         <div class="movements__row">
           <div class="movements__type movements__type--${type}">${
