3bc11655dad8a1bc30bedb2ca67db300592b6b45
Diego Rocha <drocha@griddynamics.com>
2024-08-22 11:02:28 -0600

    Displaying different contents for tab buttons

--- bankist-landing-page/script.js
+++ bankist-landing-page/script.js
@@ -82,3 +82,31 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
+
+///////////////////////////////////////
+// Tabbed component
+
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+
+  if (!clicked) return;
+
+  tabs.forEach(function (tab) {
+    tab.classList.remove("operations__tab--active");
+  });
+  clicked.classList.add("operations__tab--active");
+
+  // remove active contents
+  console.log(clicked.dataset.tab);
+  tabsContent.forEach(function (tabContent) {
+    tabContent.classList.remove("operations__content--active");
+  });
+  // activate clicked tab
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
